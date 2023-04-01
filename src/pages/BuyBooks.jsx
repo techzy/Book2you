dc149be575a1733347f936a1a5e3b157e501e322
techzy <jessjoemanoj@gmail.com>
2023-04-01 21:59:54 +0530

    Signup + google (Book2manv1.1)

--- src/pages/BuyBooks.jsx
+++ src/pages/BuyBooks.jsx
@@ -41,24 +41,24 @@ export default function BuyBooks() {
 
             </form> */}
 
-                <div className="container-fluid bg-light">
-                <div className="container py-3 ">
-                    <div class="mb-3">
-                      <label for="" class="form-label ">Title</label>
-                      <input type="text"
-                        class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+                {/* <div className="container-fluid bg-light">
+                    <div className="container py-3 ">
+                        <div class="mb-3">
+                        <label for="" class="form-label ">Title</label>
+                        <input type="text"
+                            class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">City</label>
+                            <select class="form-select form-select-lg" name="" id="">
+                                <option selected>All</option>
+                                <option value="">New Delhi</option>
+                                <option value="">Istanbul</option>
+                                <option value="">Jakarta</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label">City</label>
-                        <select class="form-select form-select-lg" name="" id="">
-                            <option selected>All</option>
-                            <option value="">New Delhi</option>
-                            <option value="">Istanbul</option>
-                            <option value="">Jakarta</option>
-                        </select>
-                    </div>
-                </div>
-                </div>
+                </div> */}
                 <div className="container pt-3">
                     
                     {
