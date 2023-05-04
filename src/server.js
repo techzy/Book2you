fbcaa5e9b657f6b1231de65f28f99d8e2c5dad67
techzy <jessjoemanoj@gmail.com>
2023-05-03 23:16:43 -0600

    some issue

--- src/server.js
+++ src/server.js
@@ -56,7 +56,7 @@ export async function addBook(user) {
                 // Observe state change events such as progress, pause, and resume
                 // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is ' + progress + '% done');
+                alert('Upload is ' + progress + '% done');
             },
             (error) => {
                 console.log(error)
