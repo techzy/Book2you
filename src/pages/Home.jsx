49b155a79cea0207967a7998d3a3dddd7b44f11e
techzy <jessjoemanoj@gmail.com>
2023-05-09 23:02:19 -0600

    Homepage better :

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,41 @@
 import '../css/Home.css'
 import { Link } from 'react-router-dom';
-import bookCircle from '../images/mobilegraph.jpeg'
 function Home() {
   return (
-    <>
+    <div >
       
-      <div className="d-flex justify-content-center">
-        <p>This is a platform where you can buy and sell second hand books</p>
+      <div className="d-flex justify-content-end flex-column flex-md-row" style={{height:'90vh'}}>
+        
+        <div className="flex-fill card br-0 border-dark mx-3 my-4">
+          <div className="card-body">
+            <Link to='/buy'>
+              <h4 className="card-title">Buy books</h4>
+            </Link>
+
+
+              <p>
+                <strong>Can't afford a brand new book?</strong>
+                <br />
+                <br />
+                Buy your book today for cheap price
+              </p>
+          </div>
+        </div>
+        <div className="flex-fill d-flex justify-content-center card br-0 border-dark mx-3 my-4">
+          <div className="card-body">
+            <Link to="/sell">
+              <h4 className="card-title">Sell books</h4></Link>
+              <p>
+                <strong>Have dozens of old books just sitting around?</strong>
+                <br />
+                <br />
+                Sell your books today and make some quick cash
+              </p>
+          </div>
+        </div>
       </div>
 
-    </>
+    </div>
   );
 }
 
